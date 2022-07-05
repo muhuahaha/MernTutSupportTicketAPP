3122fe8d63ed1c5173650fab540a48c77422dc76
Young <kimyou2019@gmail.com>
2022-07-05 03:43:19 +0200

    add authentication

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ const PORT = process.env.PORT || 8000;
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import './styles/style.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
